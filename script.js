1b420d43dbb8a188c6efe2a97e4a37134239fe6a
Gonzalo Mainardi <gonzalomainardi21@gmail.com>
2022-10-25 18:47:18 -0300

    detalles finales

--- script.js
+++ script.js
@@ -10,19 +10,22 @@ else if (opcion == 3) {
     opcion = "ZAPATILLA";
 }
 
-
-console.log("USTED ELIJIO COMPRAR: " + opcion);
-
 switch (opcion) {
     case "REMERA":
         let precioremera = 3500
         let remeraColor = ConsultarColor();
         let remeraTalle = ConsultarTalle();
         let remeraCantidad = ConsultarCantidad();
-        alert("USTED ELIJIO COMPRAR: " + remeraCantidad + " " + opcion + " COLOR: " + remeraColor + " TALLE: " + remeraTalle);
+        console.log("USTED ELIJIO COMPRAR: " + remeraCantidad + " " + opcion + " COLOR: " + remeraColor + " TALLE: " + remeraTalle);
         let remeraFormaPago = ConsultarFormaPago();
         let remeraMonto = CalcularMonto(precioremera,remeraCantidad);
-        console.log("EL MONTO FINAL A ABONAR ES: " + remeraMonto);
+        if (remeraFormaPago == 1 || remeraFormaPago == 2) {
+            console.log("EL MONTO FINAL A ABONAR ES: " + "$" + remeraMonto);
+        }
+        else if (remeraFormaPago == 3) {
+            let montoPorCuota = calcularCuota(remeraMonto)
+            console.log("EL VALOR DE CADA CUOTA ES: " + "$" + montoPorCuota)
+        }
         break;
 
     case "PANTALON":
@@ -30,10 +33,16 @@ switch (opcion) {
         let pantalonColor = ConsultarColor();
         let pantalonTalle = ConsultarTalle();
         let pantalonCantidad = ConsultarCantidad();
-        alert("USTED ELIJIO COMPRAR: " + pantalonCantidad + " " + opcion + " COLOR: " + pantalonColor + " TALLE: " + pantalonTalle);
+        console.log("USTED ELIJIO COMPRAR: " + pantalonCantidad + " " + opcion + " COLOR: " + pantalonColor + " TALLE: " + pantalonTalle);
         let pantalonFormaPago = ConsultarFormaPago();
         let pantalonMonto = CalcularMonto(preciopantalon,pantalonCantidad);
-        console.log("EL MONTO FINAL A ABONAR ES: " + pantalonMonto)
+        if (pantalonFormaPago == 1 || pantalonFormaPago == 2) {
+            console.log("EL MONTO FINAL A ABONAR ES: " + "$" + pantalonMonto);
+        }
+        else if (pantalonFormaPago == 3) {
+            let montoPorCuota = calcularCuota(pantalonMonto)
+            console.log("EL VALOR DE CADA CUOTA ES: " + "$" + montoPorCuota)
+        }
         break;
 
     case "ZAPATILLA":
@@ -41,10 +50,16 @@ switch (opcion) {
             let zapatillaColor = ConsultarColor();
             let zapatillaTalle = ConsultarTalle();
             let zapatillaCantidad = ConsultarCantidad();
-            alert("USTED ELIJIO COMPRAR: " + zapatillaCantidad + " " + opcion + " COLOR: " + zapatillaColor + " TALLE: " + zapatillaTalle);
+            console.log("USTED ELIJIO COMPRAR: " + zapatillaCantidad + " " + opcion + " COLOR: " + zapatillaColor + " TALLE: " + zapatillaTalle);
             let zapatillaFormaPago = ConsultarFormaPago();
             let zapatillaMonto = CalcularMonto(zapatillaPrecio,zapatillaCantidad);
-            console.log("EL MONTO FINAL A ABONAR ES: " + zapatillaMonto)
+            if (zapatillaFormaPago == 1 || zapatillaFormaPago == 2) {
+                console.log("EL MONTO FINAL A ABONAR ES: " + "$" + zapatillaMonto);
+            }
+            else if (zapatillaFormaPago == 3) {
+                let montoPorCuota = calcularCuota(zapatillaMonto)
+                console.log("EL VALOR DE CADA CUOTA ES: " + "$" + montoPorCuota)
+            }
             break;  
             
     default:
@@ -91,16 +106,16 @@ function ConsultarCantidad () {
 function ConsultarFormaPago () {
     let forma = prompt("ELIJA FORMA DE PAGO: 1-EFECTIVO | 2-DEBITO | 3-CREDITO")
     if (forma == 1) {
-        console.log("USTED DECIDIO ABONAR EN EFECTIVO")
+        console.log("USTED DECIDIO ABONAR EN EFECTIVO");
     }
     else if (forma == 2){
-        console.log("USTED DECIDIO ABONAR CON TARJETA DE DEBITO")
+        console.log("USTED DECIDIO ABONAR CON TARJETA DE DEBITO");
     }
     else if (forma == 3){
-        console.log("USTED DECIDIO ABONAR CON TARJETA DE CREDITO")
+       
     }
     else {
-        alert("INGRESO FORMA DE PAGO EQUIVOCADA, INTENTE NUEVAMENTE")
+        alert("INGRESO FORMA DE PAGO EQUIVOCADA, INTENTE NUEVAMENTE");
         ConsultarFormaPago();
     }
     return forma;
@@ -109,4 +124,24 @@ function ConsultarFormaPago () {
 function CalcularMonto (precio,cantidad) {
     let montofinal = precio * cantidad;
     return montofinal;
+}
+
+function calcularCuota (monto) {
+    let cuotas = prompt("ELIJA CANTIDAD DE CUOTAS: 1 CUOTA SIN INTERES | 3 CUOTAS SIN INTERES | 6 COUTAS CON 10% DE RECARGO");
+    if (cuotas == 1) {
+        console.log("USTED ELIJIO ABONAR CON TARJETA DE CREDITO EN 1 CUOTA SIN INTERES")
+        console.log("EL MONTO FINAL ABONAR ES: " + "$" + monto)
+        montoCuota = monto
+    }
+    else if (cuotas == 3) {
+        console.log("USTED ELIJIO ABONAR CON TARJETA DE CREDITO EN 3 CUOTAS SIN INTERES")
+        console.log("EL MONTO FINAL ABONAR ES: " + "$" + monto)
+        montoCuota = monto/3
+    }
+    else if (cuotas == 6) {
+        console.log("USTED ELIJIO ABONAR CON TARJETA DE CREDITO EN 6 CUOTAS SIN INTERES CON 10% DE RECARGO")
+        console.log("EL MONTO FINAL A ABONAR ES: " + "$" + monto*1.10)
+        montoCuota = (monto*1.10)/6
+    }
+    return montoCuota
 }
