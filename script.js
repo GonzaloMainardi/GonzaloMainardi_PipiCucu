27df30e07fdcfc9b8dbc4f32ffa61bcc49e56ba5
Gonzalo Mainardi <gonzalomainardi21@gmail.com>
2022-11-05 01:35:29 -0300

    segunda entrega

--- script.js
+++ script.js
@@ -1,87 +1,81 @@
 
-let articuloElegido = prompt("INGRESE EL NUMERO DEL ARTICULO QUE DESEA COMPRAR: \n 1) REMERA \n 2) PANTALON \n 3) ZAPATILLA");
+class Producto {
 
-if (articuloElegido == 1) {
-    articuloElegido = "REMERA";
+    constructor(id,nombre,marca,precio,stock){
+        this.id = id
+        this.nombre = nombre
+        this.marca = marca
+        this.precio = precio
+        this.stock = stock
+    }
 }
-else if (articuloElegido == 2) {
-    articuloElegido = "PANTALON";
-}
-else if (articuloElegido == 3) {
-    articuloElegido = "ZAPATILLA";
-}
+
+const producto1 = new Producto(01,"REMERA","LACOSTE",2800,125)
+const producto2 = new Producto(02,"PANTALON","LEVIS",8900,85)
+const producto3 = new Producto(03,"ZAPATILLA","CONVERSE",14500,50)
+
+const BBDD = [producto1,producto2,producto3]
+
+
+let articuloElegido = Number(prompt("INGRESE EL NUMERO DEL ARTICULO QUE DESEA COMPRAR: \n 1) REMERA \n 2) PANTALON \n 3) ZAPATILLA"));
 
 do {
     switch (articuloElegido) {
-        case "REMERA":
-            let precioremera = 3500
+        case 1:
+            let precioRemera = BBDD[0].precio;
             let remeraColor = ConsultarColor();
-            let remeraTalle = ConsultarTalle();
-            let remeraCantidad = ConsultarCantidad(articuloElegido);
-            console.log("USTED ELIJIO COMPRAR: " + remeraCantidad + " " + articuloElegido + " COLOR: " + remeraColor + " TALLE: " + remeraTalle);
+            let remeraTalle = ConsultarTalle(BBDD[0].nombre);
+            let remeraCantidad = ConsultarCantidad(BBDD[0].nombre);
             let remeraFormaPago = ConsultarFormaPago();
-            let remeraMonto = CalcularMonto(precioremera,remeraCantidad);
+            let remeraMonto = CalcularMonto(precioRemera,remeraCantidad);
             if (remeraFormaPago == 1 || remeraFormaPago == 2) {
-                alert("EL MONTO FINAL A ABONAR ES: " + "$" + remeraMonto);
-                console.log("EL MONTO FINAL A ABONAR ES: " + "$" + remeraMonto);
-                alert("MUCHAS GRACIAS POR ELEJIRNOS");
-                articuloElegido = false;
+                mostrarTicket(remeraCantidad,BBDD[0].nombre,BBDD[0].marca,remeraColor,remeraTalle,remeraMonto)
+                articuloElegido = seguirComprando();
         }
             else if (remeraFormaPago == 3) {
                 let montoPorCuota = calcularCuota(remeraMonto)
+                mostrarTicket(remeraCantidad,BBDD[0].nombre,BBDD[0].marca,remeraColor,remeraTalle,remeraMonto)
                 alert("EL VALOR DE CADA CUOTA ES: " + "$" + montoPorCuota);
-                console.log("EL VALOR DE CADA CUOTA ES: " + "$" + montoPorCuota);
-                alert("MUCHAS GRACIAS POR ELEJIRNOS");
-                articuloElegido = false;
+                articuloElegido = seguirComprando();
         }
             break;
     
-        case "PANTALON":
-            let preciopantalon = 8200
+        case 2:
+            let precioPantalon = BBDD[1].precio;
             let pantalonColor = ConsultarColor();
-            let pantalonTalle = ConsultarTalle();
-            let pantalonCantidad = ConsultarCantidad(articuloElegido);
-            console.log("USTED ELIJIO COMPRAR: " + pantalonCantidad + " " + articuloElegido + " COLOR: " + pantalonColor + " TALLE: " + pantalonTalle);
+            let pantalonTalle = ConsultarTalle(BBDD[1].nombre);
+            let pantalonCantidad = ConsultarCantidad(BBDD[1].nombre);
             let pantalonFormaPago = ConsultarFormaPago();
-            let pantalonMonto = CalcularMonto(preciopantalon,pantalonCantidad);
+            let pantalonMonto = CalcularMonto(precioPantalon,pantalonCantidad);
             if (pantalonFormaPago == 1 || pantalonFormaPago == 2) {
-                alert("EL MONTO FINAL A ABONAR ES: " + "$" + pantalonMonto);
-                console.log("EL MONTO FINAL A ABONAR ES: " + "$" + pantalonMonto);
-                alert("MUCHAS GRACIAS POR ELEJIRNOS");
-                articuloElegido = false;
+                mostrarTicket(pantalonCantidad,BBDD[1].nombre,BBDD[1].marca,pantalonColor,pantalonTalle,pantalonMonto)
+                articuloElegido = seguirComprando();
 
         }
             else if (pantalonFormaPago == 3) {
                 let montoPorCuota = calcularCuota(pantalonMonto)
+                mostrarTicket(pantalonCantidad,BBDD[1].nombre,BBDD[1].marca,pantalonColor,pantalonTalle,pantalonMonto)
                 alert("EL VALOR DE CADA CUOTA ES: " + "$" + montoPorCuota);
-                console.log("EL VALOR DE CADA CUOTA ES: " + "$" + montoPorCuota);
-                alert("MUCHAS GRACIAS POR ELEJIRNOS");
-                articuloElegido = false;
-
+                articuloElegido = seguirComprando();
         }
             break;
     
-        case "ZAPATILLA":
-                let zapatillaPrecio = 15000
+        case 3:
+                let zapatillaPrecio = BBDD[2].precio
                 let zapatillaColor = ConsultarColor();
-                let zapatillaTalle = ConsultarTalle();
-                let zapatillaCantidad = ConsultarCantidad(articuloElegido);
-                console.log("USTED ELIJIO COMPRAR: " + zapatillaCantidad + " " + articuloElegido + " COLOR: " + zapatillaColor + " TALLE: " + zapatillaTalle);
+                let zapatillaTalle = ConsultarTalle(BBDD[2].nombre);
+                let zapatillaCantidad = ConsultarCantidad(BBDD[2].nombre);
                 let zapatillaFormaPago = ConsultarFormaPago();
                 let zapatillaMonto = CalcularMonto(zapatillaPrecio,zapatillaCantidad);
                 if (zapatillaFormaPago == 1 || zapatillaFormaPago == 2) {
-                    alert("EL MONTO FINAL A ABONAR ES: " + "$" + zapatillaMonto);
-                    console.log("EL MONTO FINAL A ABONAR ES: " + "$" + zapatillaMonto);
-                    alert("MUCHAS GRACIAS POR ELEJIRNOS");
-                    articuloElegido = false;
-
+                    mostrarTicket(zapatillaCantidad,BBDD[2].nombre,BBDD[2].marca,zapatillaColor,zapatillaTalle,zapatillaMonto)
+                    articuloElegido = seguirComprando();
                 }
                 else if (zapatillaFormaPago == 3) {
                     let montoPorCuota = calcularCuota(zapatillaMonto);
+                    mostrarTicket(zapatillaCantidad,BBDD[2].nombre,BBDD[2].marca,zapatillaColor,zapatillaTalle,zapatillaMonto)
                     alert("EL VALOR DE CADA CUOTA ES: " + "$" + montoPorCuota);
-                    console.log("EL VALOR DE CADA CUOTA ES: " + "$" + montoPorCuota);
-                    alert("MUCHAS GRACIAS POR ELEJIRNOS");
-                    articuloElegido = false;
+                    articuloElegido = seguirComprando();
     
                 }
                 break;  
@@ -91,11 +85,17 @@ do {
             break;
     }
     
-} while (articuloElegido === "REMERA" || articuloElegido == "PANTALON" || articuloElegido == "ZAPATILLA")
+} while (articuloElegido === 1 || articuloElegido == 2 || articuloElegido == 3)
 
-function ConsultarTalle () {
-    let talle = prompt("INGRESE EL TALLE DE PREFERENCIA: (S,M,L,XL,XXL)");
-    return talle.toUpperCase();
+function ConsultarTalle (articulo) {
+    if (articulo === "ZAPATILLA") {
+        let talle = prompt("INGRESE EL TALLE DE PREFERENCIA: ");
+        return talle;    
+    }
+    else {
+        let talle = prompt("INGRESE EL TALLE DE PREFERENCIA: (S,M,L,XL,XXL)");
+        return talle.toUpperCase();
+    }
 }
 
 function ConsultarColor () {
@@ -123,8 +123,43 @@ function ConsultarColor () {
 }
 
 function ConsultarCantidad (articulo) {
-    let cant = parseInt(prompt("INGRESE LA CANTIDAD DE " + articulo + " DESEADA:"));
-    return cant;
+    if (articulo === "REMERA") {
+        let cant = parseInt(prompt("INGRESE LA CANTIDAD DE " + articulo + " DESEADA (STOCK DISPONIBLE: " + BBDD[0].stock + ")"));
+        if (cant > BBDD[0].stock) {
+            alert("NO DISPONEMOS DE ESE STOCK")
+            ConsultarCantidad(articulo)
+        }
+        else{
+            BBDD[0].stock = BBDD[0].stock - cant;
+            return cant;
+        }
+    }
+    else if (articulo === "PANTALON") {
+        let cant = parseInt(prompt("INGRESE LA CANTIDAD DE " + articulo + " DESEADA (STOCK DISPONIBLE: " + BBDD[1].stock + ")"));
+
+        if (cant > BBDD[1].stock) {
+            alert("NO DISPONEMOS DE ESE STOCK")
+            ConsultarCantidad(articulo)
+        }
+        else{
+            BBDD[1].stock = BBDD[1].stock - cant;
+            return cant;
+
+        }
+    }
+    if (articulo === "ZAPATILLA") {
+        let cant = parseInt(prompt("INGRESE LA CANTIDAD DE " + articulo + " DESEADA (STOCK DISPONIBLE: " + BBDD[2].stock + ")"));
+
+        if (cant > BBDD[2].stock) {
+            alert("NO DISPONEMOS DE ESE STOCK")
+            ConsultarCantidad(articulo)
+        }
+        else{
+            BBDD[2].stock = BBDD[2].stock - cant;
+            return cant;
+
+        }
+    }
 }
 
 function ConsultarFormaPago () {
@@ -154,20 +189,14 @@ function calcularCuota (monto) {
     let cuotas = prompt("ELIJA CANTIDAD DE CUOTAS: \n 1 CUOTA SIN INTERES \n 3 CUOTAS SIN INTERES \n 6 COUTAS CON 10% DE RECARGO");
     if (cuotas == 1) {
         console.log("USTED ELIJIO ABONAR CON TARJETA DE CREDITO EN 1 CUOTA SIN INTERES");
-        alert("EL MONTO FINAL A ABONAR ES: " + "$" + monto);
-        console.log("EL MONTO FINAL A ABONAR ES: " + "$" + monto);
         montoCuota = monto
     }
     else if (cuotas == 3) {
         console.log("USTED ELIJIO ABONAR CON TARJETA DE CREDITO EN 3 CUOTAS SIN INTERES");
-        alert("EL MONTO FINAL A ABONAR ES: " + "$" + monto);
-        console.log("EL MONTO FINAL A ABONAR ES: " + "$" + monto);
         montoCuota = monto/3
     }
     else if (cuotas == 6) {
         console.log("USTED ELIJIO ABONAR CON TARJETA DE CREDITO EN 6 CUOTAS SIN INTERES CON 10% DE RECARGO");
-        alert("EL MONTO FINAL A ABONAR ES: " + "$" + monto*1.10);
-        console.log("EL MONTO FINAL A ABONAR ES: " + "$" + monto*1.10);
         montoCuota = (monto*1.10)/6
     }
     else {
@@ -176,4 +205,25 @@ function calcularCuota (monto) {
 
     }
     return montoCuota
+}
+
+
+function seguirComprando() {
+
+    var opcion = confirm("¿DESEA REALIZAR OTRA COMPRA?")
+    if (opcion == true) {
+        let articuloElegido = Number(prompt("INGRESE EL NUMERO DEL ARTICULO QUE DESEA COMPRAR: \n 1) REMERA \n 2) PANTALON \n 3) ZAPATILLA"));
+        return articuloElegido;
+    }
+    else {
+        alert("MUCHAS GRACIAS POR ELEJIRNOS");
+    }
+
+}
+
+function mostrarTicket (cantidad,nombre,marca,color,talle,monto) {
+    console.log("***** TICKET DE COMPRA *****")
+    console.log("USTED ELIJIO COMPRAR: " + cantidad + " " + nombre + " MARCA: " + marca +  " COLOR: " + color + " TALLE: " + talle);
+    console.log("EL MONTO FINAL A ABONAR ES: " + "$" + monto);
+    console.log("****************************");
 }
